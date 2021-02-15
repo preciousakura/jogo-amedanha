91b6e989752beb19e4ec065fb6262f52921b2885
isa <isawithans@gmail.com>
2021-02-15 20:50:29 -0300

    nada

--- rascunho das funcoes/testedef.c
+++ rascunho das funcoes/testedef.c
@@ -8,47 +8,39 @@ int contar_Caractere(char *res){
     }
     return tamanho;
 }
-int validar_jogadores( char *pont ,char nome_de_jogadores[12][10]){
-    
-    int N,i = 0;
-    printf("Digite o numero de jogadores: "); // numero de jogadores deve ser maior q 10 e menor q 2;
-    scanf("%d",&N); //Numero de jogadores
 
-    while ((N < 2)||(N>10)) {
+
+int validar_jogadores(char **nome_de_jogadores, int linha){
+    
+    int numero_de_jogadores, i = 0;
+
+    printf("Digite o numero de jogadores: "); // numero de jogadores deve ser maior q 10 e menor q 2;
+    scanf("%d", &numero_de_jogadores); //Numero de jogadores
+
+    while ((numero_de_jogadores < 2)||(numero_de_jogadores>10)) {
         printf("Numero de jogadores invalido!\nDigite novamente: ");
-        scanf("%d",&N);
+        scanf("%d",&numero_de_jogadores);
     }
 
-    for(i = 0; i < N ; i++ )
-    {
+    for(i = 0; i < numero_de_jogadores ; i++){
         
         printf("Qual eh o nome do jogador %d: ",i+1);
-        scanf("%s",nome_de_jogadores[i]);
-       
-            while((contar_Caractere(nome_de_jogadores[i])>12) || (contar_Caractere(nome_de_jogadores[i]) == 0)){
-                printf("Nome muito longo! \nDigite o nome do jogador %d novamente:",i+1);
-                scanf("%s",&nome_de_jogadores[i]);
-                }  
-            pont = nome_de_jogadores[i];
-            }
-    return N;
-            }
-
-
-            //ajuda com ponteiros
-            //pedir para digitar o nome novamente;
-        
-   
-     
+        scanf("%s", *nome_de_jogadores);
+        while((contar_Caractere(nome_de_jogadores[i]) > 12) || (contar_Caractere(nome_de_jogadores[i]) == 0)){
+            printf("Nome muito longo! \nDigite o nome do jogador %d novamente:",i+1);
+            scanf("%s", *nome_de_jogadores);
+        }  
+    }
     
+    return numero_de_jogadores;
+}
 
 int main(){
-    char *pont_para_nome_de_jogadores;
-    char nome_de_jogadores[12][10];
-    int numero_jogadores;
-    numero_jogadores = validar_jogadores(pont_para_nome_de_jogadores , nome_de_jogadores);
+    int linha = 0;
+    char *nome_de_jogadores[10][12];
+    int numero_jogadores = validar_jogadores(nome_de_jogadores[linha], linha++);
+
     for (int i = 0; i < numero_jogadores ; i++ ){//i menor q tamanho, 5 é teste;
         printf("%s\n",nome_de_jogadores[i]);
     }
-
 }
