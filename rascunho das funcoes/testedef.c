8bbd53eec6f33923d0f0b38d9ad12d7ccc16d7e7
Matheusriale <Matheusriale@alu.ufc.br>
2021-02-16 16:34:15 -0300

    matheus começar a fazer funçao de marcar tempo

--- rascunho das funcoes/testedef.c
+++ rascunho das funcoes/testedef.c
@@ -10,34 +10,38 @@ int contar_Caractere(char *res){
 }
 
 
-int validar_jogadores(char **nome_de_jogadores, int linha){
+int validar_jogadores(char *nome_de_jogadores, int linha){
+    int numero_de_jogadores;
+    int i = 0;
     
-    int numero_de_jogadores, i = 0;
 
     printf("Digite o numero de jogadores: "); // numero de jogadores deve ser maior q 10 e menor q 2;
     scanf("%d", &numero_de_jogadores); //Numero de jogadores
 
-    while ((numero_de_jogadores < 2)||(numero_de_jogadores>10)) {
+    while ((numero_de_jogadores < 2) || (numero_de_jogadores>10)) {
         printf("Numero de jogadores invalido!\nDigite novamente: ");
-        scanf("%d",&numero_de_jogadores);
+        scanf("%d" , &numero_de_jogadores);
     }
 
+    numero_de_jogadores = 5;
+    printf("%d",numero_de_jogadores);
+    /*
     for(i = 0; i < numero_de_jogadores ; i++){
         
         printf("Qual eh o nome do jogador %d: ",i+1);
-        scanf("%s", *nome_de_jogadores);
+        scanf("%s", *nome_de_jogadores[i]);
         while((contar_Caractere(nome_de_jogadores[i]) > 12) || (contar_Caractere(nome_de_jogadores[i]) == 0)){
             printf("Nome muito longo! \nDigite o nome do jogador %d novamente:",i+1);
             scanf("%s", *nome_de_jogadores);
         }  
-    }
+    }*/
     
     return numero_de_jogadores;
 }
 
 int main(){
     int linha = 0;
-    char *nome_de_jogadores[10][12];
+    char *nome_de_jogadores[12][10];
     int numero_jogadores = validar_jogadores(nome_de_jogadores[linha], linha++);
 
     for (int i = 0; i < numero_jogadores ; i++ ){//i menor q tamanho, 5 é teste;
