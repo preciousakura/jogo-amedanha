0a71011451cb80dade8cf8aea0bd273535a46f60
Isabel Cristina <isabel.cristina@insight.ufc.br>
2021-03-09 12:51:11 -0300

    nome da equipe nos comentarios

--- projeto final/main.c
+++ projeto final/main.c
@@ -1,3 +1,12 @@
+/*
+Trabalho Intermediário – Jogo Amedonha
+Disciplina Programação (CK0226) – Semestre 2020.2
+Equipe: Guilherme Sales Fernandes - 499756
+		Isabel Cristina de Oliveira Lopes - 493948
+		Matheus Ribeiro Alencar - 494711
+		
+*/
+
 #include "jogo.h"
 
 int main() {
